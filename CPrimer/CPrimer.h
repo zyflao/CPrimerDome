2765a612e24757fabb9082cb75f770b7a967b7a5
zyflao <zyflao@sina.com>
2024-11-04 10:42:18 +0800

    git@github.com:zyflao/CPrimerDome.git

---
+++ CPrimer/CPrimer.h
@@ -0,0 +1,12 @@
+﻿
+#pragma once
+
+#include <iostream>
+int Main() {
+    
+    int i=1;
+    // 输出 "Hello, World!"
+    std::cout << "Hello, C++Primer " << '\n';
+    std::cout << "The student Day is: " << i <<  '\n';
+    return 0;
+}
---
+++ MyFirstC++Dome/HelloWorld.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+inline int main() {
+
+     int i=100;
+    // 输出 "Hello, World!"
+    std::cout << "Hello, World! " << '\n';
+    std::cout << "The value of i is: " << i << '\n';
+    return 0;
+}
